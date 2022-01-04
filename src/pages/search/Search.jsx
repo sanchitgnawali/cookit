06611aab56098083dcfecfe8b4ea67778bf8fccd
Sanchit Gnawali <sanchit.io@Sanchits-MBP.hsd1.tx.comcast.net>
2022-01-03 23:43:54 -0600

    Fixed the parameter passing through url issue

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,20 @@
+import { useSearchParams } from "react-router-dom";
+import { useFetch } from "./../../hooks/useFetch";
+
 import "./Search.css";
 
 export default function Search() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get("q");
+
+  const url = "http://localhost:3000/recipes?q=" + query;
+
+  const { data, error, isPending } = useFetch(url, "GET");
+
   return (
     <div>
-      <h1>Search</h1>
+      <h2 className="page-title">Recipes including {query}</h2>
     </div>
   );
 }
